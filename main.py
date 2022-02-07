67c8ad185d07aadde6c818de24713bc4a413b7ca
Bonian Hu <lalalajajaja18@gmail.com>
2022-02-07 23:25:43 +0000

    Object 1 and Object 2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import HumanFriendlyTextClock as clock
 import argparse
+from datetime import datetime
+
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # args = parser.parse_args()
-    x = input()
-    print(clock.get_human_friendly_text(x))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--time", default=datetime.now().strftime("%H:%M"),
+                        help="Insert time a time in H:MM or HH:MM format, default current time")
+    
+    args = vars(parser.parse_args())
+    print(clock.get_human_friendly_text(args['time']))
