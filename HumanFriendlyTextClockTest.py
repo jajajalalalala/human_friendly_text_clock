1485b8521506ef8bc68fd6353ba459cfb6a3f819
Bonian Hu <lalalajajaja18@gmail.com>
2022-02-08 23:26:22 +0000

    modify comments

--- HumanFriendlyTextClockTest.py
+++ HumanFriendlyTextClockTest.py
@@ -32,12 +32,6 @@ class TestSum(unittest.TestCase):
         self.assertEqual(clock.get_human_friendly_text("13:30"), "13:30 Half past one")
         self.assertEqual(clock.get_human_friendly_text("00:00"), "00:00 Twelve o'clock")
 
-    # def test_sum(self):
-    #     self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-    #
-    # def test_sum_tuple(self):
-    #     self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
-
 
 if __name__ == '__main__':
     unittest.main()
